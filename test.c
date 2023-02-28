3df038c5fb92e09a5cf3d1bf0bffdcdb789a6c20
vijay-wang <ww107587@gmail.com>
2023-02-28 19:22:06 +0800

    SPI的IO口和UART2的IO口复用，将UART2相关的注释掉

---
+++ test.c
@@ -0,0 +1,24 @@
+input_unregister
+GFP_KERNEL
+http://plinux.org/lxr/http/crossref.html
+kzalloc
+spidev_setup
+copy_to_user
+alloc_chrdev_region
+MAJOR
+i2c_device_id
+cdev_add
+class_create
+struct i2c_driver
+msleep
+BIT_MASK
+struct device_node *node;
+ input_register_device
+input_set_capability
+KEY0
+input_event
+struct input_event
+struct input_dev
+input_register_device
+spi_setup
+SPI_MODE_0
